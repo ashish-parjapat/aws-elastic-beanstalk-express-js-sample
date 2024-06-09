a2126c6e0d7ba85121486fd0b0bbd0b40b130344
Ashish Kumar <591ashish.2020@gmail.com>
2024-06-09 11:35:24 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first ci/cd pipeline'));
+app.get('/', (req, res) => res.send('This is my first ci/cd pipeline and now testing my ci/cd'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
